738831f03367f882d2be9938e682df3d188266da
Ron <laniado.ron@gmail.com>
2022-01-04 02:18:04 -0500

    use custom collapsible component for faq

---
+++ components/Collapsible.js
@@ -0,0 +1,32 @@
+import styles from "./collapsible.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; 
+import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons"; 
+import { useState } from "react";
+
+function Collapsible(props) {
+
+    const [isOpen, setIsOpen] = useState(props.isOpen || false);
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.title} onClick={() => setIsOpen(!isOpen)}>
+                <FontAwesomeIcon icon={isOpen ? faMinus : faPlus} size="lg" className={styles.titleIcon}/>
+                <div className={styles.titleText}>{props.title}</div>
+            </div>
+            <div 
+            className={styles.content}
+            style={{  
+                        transform: isOpen ? "scaleY(1)" : "scaleY(0)",
+                        height: isOpen ? "100%" : "0",
+                        paddingBottom: isOpen ? "50px" : "0"
+                        }}>
+                <div className={styles.vertLine}></div>
+                <div className={styles.contentText}>
+                    {props.body}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Collapsible;
--- components/FrequentlyAsked.js
+++ components/FrequentlyAsked.js
@@ -1,88 +1,30 @@
 import styles from "./frequentlyasked.module.css";
-import Collapsible from "react-collapsible";
+import Collapsible from "./Collapsible";
 function FrequentlyAsked() {
     return (
         <div className={styles.container} id="faq">
             <h1 className={styles.title}>Frequently Asked Questions</h1>
             <div className={styles.collapsibles}>
-                <div>
+                    <Collapsible  
+                        title="What is HackBU?" 
+                        body="HackBU is a student-run organization at Binghamton University. We aim to create a community where students can explore technology outside the classroom. We mentor students, hold weekly workshops, and, most importantly, share our interest in programming and problem-solving."
+                        isOpen={true}/>
+                    <Collapsible  
+                        title="What happens at a hackathon?"
+                        body='At a hackathon, "hackers" form teams with the intention of creating the best type of program (e.g. web app, iPhone app) or hardware hack within a certain period of time.'/>
+                    <Collapsible
+                        title="Who can attend?"
+                        body="This event is open to all students 18 and older. You don't need to be a computer science major or even a Binghamton University student to participate!" />
+                    <Collapsible
+                        title="How much does it cost?"
+                        body="HackBU is a free event! This means that all you'll need is a computer to work on, an internet connection, and an idea! In fact, we'll be giving you free swag at the end of the hackathon for participating!" />
+                    <Collapsible
+                        title="Do I need a team or idea to sign up?"
+                        body="No! We recommend registering with friends, but there will be opportunities to brainstorm with and meet other participants at the hackathon." />
                     <Collapsible 
-                        trigger="What is HackBU?" 
-                        triggerClassName={styles.trigger} 
-                        triggerOpenedClassName={styles.triggerOpened}
-                        transitionTime={200}
-                        open={true}
-                        className={styles.collapsible}
-                        openedClassName={styles.collapsible}
-                        easing="ease-in">
-                        <div className={styles.collapseContent}>
-                            HackBU is a student-run organization at Binghamton University. We aim to create a community where students can explore technology outside the classroom. We mentor students, hold weekly workshops, and, most importantly, share our interest in programming and problem-solving.
-                        </div>
-                    </Collapsible>
-                    <Collapsible 
-                        trigger="What happens at a hackathon?" 
-                        triggerClassName={styles.trigger} 
-                        triggerOpenedClassName={styles.triggerOpened}
-                        transitionTime={200}
-                        className={styles.collapsible}
-                        openedClassName={styles.collapsible}
-                        easing="ease-in">
-                        <div className={styles.collapseContent}>
-                            At a hackathon, "hackers" form teams with the intention of creating the best type of program (e.g. web app, iPhone app) or hardware hack within a certain period of time.
-                        </div>
-                    </Collapsible>
-                     <Collapsible 
-                        trigger="Do I need a team or idea to sign up?" 
-                        triggerClassName={styles.trigger} 
-                        triggerOpenedClassName={styles.triggerOpened}
-                        transitionTime={200}
-                        className={styles.collapsible}
-                        openedClassName={styles.collapsible}
-                        easing="ease-in">
-                        <div className={styles.collapseContent}>
-                            No! We recommend registering with friends, but there will be opportunities to brainstorm with and meet other participants at the hackathon.
-                        </div>
-                    </Collapsible>
-                </div>
-                <div>
-                    <Collapsible 
-                        trigger="Who can attend?" 
-                        triggerClassName={styles.trigger} 
-                        triggerOpenedClassName={styles.triggerOpened}
-                        transitionTime={200}
-                        className={styles.collapsible}
-                        open={true}
-                        openedClassName={styles.collapsible}
-                        easing="ease-in">
-                        <div className={styles.collapseContent}>
-                            This event is open to all students 18 and older. You don't need to be a computer science major or even a Binghamton University student to participate!
-                        </div>
-                    </Collapsible>
-                    <Collapsible 
-                        trigger="What if I'm a newbie?" 
-                        triggerClassName={styles.trigger} 
-                        triggerOpenedClassName={styles.triggerOpened}
-                        transitionTime={200}
-                        className={styles.collapsible}
-                        openedClassName={styles.collapsible}
-                        easing="ease-in">
-                        <div className={styles.collapseContent}>
-                            Don't worry, beginners are welcome! Come to HackBU's weekly workshops where we teach you how to code. Additionally, we will be hosting a few introductory workshops during the hackathon to help you get started.
-                        </div>
-                    </Collapsible>
-                    <Collapsible 
-                        trigger="Will there be food?" 
-                        triggerClassName={styles.trigger} 
-                        triggerOpenedClassName={styles.triggerOpened}
-                        transitionTime={200}
-                        className={styles.collapsible}
-                        openedClassName={styles.collapsible}
-                        easing="ease-in">
-                        <div className={styles.collapseContent}>
-                           Unfortunately, as the event is virtual this year, food and snacks will not be provided.
-                        </div>
-                    </Collapsible>
-                </div>
+                        title="What if I'm a newbie?"
+                        body="This is going to be our most beginner-friendly event yet. We are going to have workshops both before and during the hackathon in order to get you in a position to be able to work on a project. We are hoping for any student of any major to be able to participate in our hackathon." />
+                    
             </div>
         </div>
     )
