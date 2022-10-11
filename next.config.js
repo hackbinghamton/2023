b36db1d480e277b411c15cd9d8013ba8d824bc3a
Koopa <codingkoopa@gmail.com>
2022-10-11 14:11:58 -0400

    config: rm redirects

--- next.config.js
+++ next.config.js
@@ -7,15 +7,6 @@ module.exports = {
 
     return config;
   },
-  async redirects() {
-    return [
-      {
-        source: "/discord",
-        destination: "https://discord.gg/Xka5uUh",
-        permanent: true,
-      },
-    ];
-  },
   basePath: '/2023',
   // https://stackoverflow.com/a/70047180
   images: {
