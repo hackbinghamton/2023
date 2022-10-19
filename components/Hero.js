ad18890ad7e637925578bf22e3543f60877bb82b
Ron <laniado.ron@gmail.com>
2022-10-19 13:47:03 -0400

    Make site content configurable

--- components/Hero.js
+++ components/Hero.js
@@ -1,33 +1,36 @@
-import Image from "next/image"
+import Image from "next/image";
 
-import styles from "./hero.module.css"
+import styles from "./hero.module.css";
 
-import heroImage from "../public/assets/heroImage.webp"
-
-import constants from "../public/constants.js"
-
+import constants from "../public/constants.js";
 
 function Hero() {
-
-    return (
-        <div className={styles.container}>
-            <div className={styles.leftContainer}>
-                <h1 className={styles.heroTitle}>Hack. <br /> Be You.</h1>
-                <p className={styles.heroSubtitle}>HackBU 2022 is <b>Binghamton University’s</b> 9th annual student-run hackathon. This year’s virtual hackathon will be hosted from February 5th-6th, over on our Discord server.</p>
-                <button 
-                    className={styles.register} 
-                    onClick={() => window.open(constants.REGISTRATION_LINK)}
-                >Register Now</button>
-            </div>
-            <div className={styles.rightContainer}>
-                <Image 
-                    src={heroImage} 
-                    alt="HackBU 2022 Hackathon"
-                    priority
-                />
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.container}>
+      <div className={styles.leftContainer}>
+        <h1 className={styles.heroTitle}>
+          Hack. <br /> Be You.
+        </h1>
+        <p
+          className={styles.heroSubtitle}
+          dangerouslySetInnerHTML={{ __html: constants.HERO_SUBTITLE }}
+        ></p>
+        <button
+          className={styles.register}
+          onClick={() => window.open(constants.REGISTRATION_LINK)}
+        >
+          Register Now
+        </button>
+      </div>
+      <div className={styles.rightContainer}>
+        <Image
+          src={constants.HERO_IMAGE}
+          alt="HackBU 2022 Hackathon"
+          priority
+        />
+      </div>
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;
--- components/Sponsors.js
+++ components/Sponsors.js
@@ -2,26 +2,28 @@ import styles from "./sponsors.module.css";
 import Image from "next/image";
 import Link from "next/link";
 
-import Raymond from "../public/assets/logos/raymond.png";
-import RubyLaw from "../public/assets/logos/ruby-law.png";
-import AIS from "../public/assets/logos/ais.png";
-import JPMorgan from "../public/assets/logos/jpmorgan.png";
+import constants from "../public/constants.js";
 
 function Sponsors() {
-    return (
-        <div id="sponsors" className={styles.container}>
-            <h1 className={styles.title}>Our Sponsors</h1>
-            <div className={styles.sponsors}>
-                <div className={styles.imageContainer}><Image src={RubyLaw} /></div>
-                <div className={styles.imageContainer}><Image src={Raymond} /></div>
-                 <div className={styles.imageContainer}><Image src={JPMorgan} /></div>
-                  <div className={styles.imageContainer}><Image src={AIS} /></div>
-            </div>
-            <Link href="/sponsorship_packet.pdf" alt="Sponsorship Packet" target="_blank">
-                <button className={styles.button}>Interested in Sponsoring?</button>
-            </Link>
-        </div>
-    )
+  return (
+    <div id="sponsors" className={styles.container}>
+      <h1 className={styles.title}>Our Sponsors</h1>
+      <div className={styles.sponsors}>
+        {constants.SPONSORS.map((sponsor) => (
+          <div className={styles.imageContainer}>
+            <Image src={sponsor} />
+          </div>
+        ))}
+      </div>
+      <Link
+        href="/sponsorship_packet.pdf"
+        alt="Sponsorship Packet"
+        target="_blank"
+      >
+        <button className={styles.button}>Interested in Sponsoring?</button>
+      </Link>
+    </div>
+  );
 }
 
 export default Sponsors;
